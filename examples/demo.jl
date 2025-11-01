d7713cb1cfcd20bb34cffecb34d263598a4db733
yryrena <yryrena@gmail.com>
2025-11-01 21:48:16 +0800

    fix: use GeometryBasics.Point for geometry construction

--- examples/demo.jl
+++ examples/demo.jl
@@ -1,24 +1,23 @@
 ## minimal demo
 ## comments use lowercase and start with ##
-using DataFrames
-using GeometryBasics        ## point type
-using GeoInterface
+import Pkg; Pkg.activate(".")
+using DataFrames, GeoInterface, GeometryBasics
 using GeoMatch
 
 ## build treated table with wgs84 lon-lat
 t = DataFrame(id=1:3,
-              x=[116.4, 116.5, 116.6],
-              y=[39.90, 39.92, 39.95],
-              income=50 .+ 10*rand(3),
-              pop=20 .+ 5*rand(3))
+              x=[116.4,116.5,116.6],
+              y=[39.9,39.92,39.95],
+              income=50 .+ 10rand(3),
+              pop=20 .+ 5rand(3))
 t.geometry = [GeometryBasics.Point(t.x[i], t.y[i]) for i in 1:nrow(t)]
 
 ## build control table
 c = DataFrame(id=101:130,
               x=116.1 .+ rand(30),
-              y=39.7  .+ 0.6*rand(30),
-              income=48 .+ 10*rand(30),
-              pop=22 .+ 5*rand(30))
+              y=39.7 .+ 0.6rand(30),
+              income=48 .+ 10rand(30),
+              pop=22 .+ 5rand(30))
 c.geometry = [GeometryBasics.Point(c.x[i], c.y[i]) for i in 1:nrow(c)]
 
 ## run matching
@@ -30,7 +29,7 @@ res = GeoMatch.match_spatial(t, c;
     distance_metric=:haversine,
     replace=false)
 
-## show pairs
+## show first pairs
 println(first(res.pairs, 6))
 
 ## balance and report
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,12 +2,12 @@
 ## comments use lowercase and start with ##
 using Test
 using GeoMatch
-using DataFrames, GeoInterface
-using GeometryBasics
+using DataFrames, GeoInterface, GeometryBasics
 
 @testset "geomatch mvp" begin
-    t = DataFrame(id=1:2, x=[0.0, 0.1], y=[0.0, 0.1], a=[1.0, 2.0])
+    t = DataFrame(id=1:2, x=[0.0, 0.1], y=[0.0, 0.1], a=[1.0,2.0])
     t.geometry = [GeometryBasics.Point(t.x[i], t.y[i]) for i in 1:nrow(t)]
+
     c = DataFrame(id=101:104, x=rand(4), y=rand(4), a=rand(4))
     c.geometry = [GeometryBasics.Point(c.x[i], c.y[i]) for i in 1:nrow(c)]
 
